10de77f04f0740f875af3b724f86e3f919dd1e7b
the-mystery-man <workwithdondaniel@gmail.com>
2022-03-01 02:17:57 +0100

    Fixed single input form display half way

--- src/components/presentational/SingleInputForm.js
+++ src/components/presentational/SingleInputForm.js
@@ -2,9 +2,11 @@ export default function SingleInputForm({label, placeholder, buttonText}){
     return (
         <div>
             <form className = "consolas">
-                <label className = "my-2 pl-2 text-light"><b>{label}</b></label><br />
-                <input className = "sif-input" placeholder = {placeholder} />
-                <button className = "sif-button text-light">{buttonText}</button>
+                <label className = "my-2 pl-2 text-light"><b>{label}</b></label>
+                <div className = "sif-inner-container ">
+                    <input className = "sif-input" placeholder = {placeholder} />
+                    <button className = "sif-button text-light">{buttonText}</button>
+                </div>
             </form>
         </div>
     )
