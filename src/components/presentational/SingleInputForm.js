6149e3bf8d22a916bfa340e6e87b0aa8456f6076
the-mystery-man <workwithdondaniel@gmail.com>
2022-03-01 10:56:54 +0100

    Final solution to single input form

--- src/components/presentational/SingleInputForm.js
+++ src/components/presentational/SingleInputForm.js
@@ -1,11 +1,13 @@
 export default function SingleInputForm({label, placeholder, buttonText}){
     return (
         <div>
-            <form className = "consolas">
-                <label className = "my-2 pl-2 text-light"><b>{label}</b></label>
-                <div className = "sif-inner-container ">
-                    <input className = "sif-input" placeholder = {placeholder} />
-                    <button className = "sif-button text-light">{buttonText}</button>
+            <form className = "consolas  ">
+                <label className = "my-2 pl-2 text-light "><b>{label}</b></label><br />
+                <div className = " d-flex justify-content-center ">
+                    <div className = "sif-inner-container ">
+                        <input className = "sif-input" placeholder = {placeholder} />
+                        <button className = "sif-button text-light">{buttonText}</button>
+                    </div>
                 </div>
             </form>
         </div>
